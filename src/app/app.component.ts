f2fa07726b5de038b59120d09f948b50ad9394c6
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-10-14 16:03:47 -0300

    data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding';
+  n1: number = 0
+  n2: number = 0
+
+  soma(x: number, y:number){
+    return x + y;
+  }
+
+  tipoInput: string = 'password'
+  
+  mudarTipoDoInput(): void {
+    if (this.tipoInput == 'password'){
+      this.tipoInput = 'text'
+    } else {
+      this.tipoInput = 'password'
+    }
+  }
 }
