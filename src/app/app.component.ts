b080b27d18ff7567529b01b0429ac5dc19232786
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-10-18 17:50:16 -0300

    cartaoCredito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
-  n1: number = 0
-  n2: number = 0
 
-  soma(x: number, y:number){
-    return x + y;
-  }
+  nomeCartao: string = ''
+  numCartao: number = 0
+  mes: number = 0
+  ano: number = 0
+  cvc: number = 0
+  // title = 'data-binding';
+  // n1: number = 0
+  // n2: number = 0
 
-  tipoInput: string = 'password'
+  // soma(x: number, y:number){
+  //   return x + y;
+  // }
+
+  // tipoInput: string = 'password'
   
-  mudarTipoDoInput(): void {
-    if (this.tipoInput == 'password'){
-      this.tipoInput = 'text'
-    } else {
-      this.tipoInput = 'password'
-    }
-  }
+  // mudarTipoDoInput(): void {
+  //   if (this.tipoInput == 'password'){
+  //     this.tipoInput = 'text'
+  //   } else {
+  //     this.tipoInput = 'password'
+  //   }
+  // }
 }
